142dfb23e7f16f086859f270378ee68db707d984
Sofia Nguy <sofianguy@gmail.com>
2015-07-19 21:40:30 -0700

    minor change -- deleted comment

--- application.py
+++ application.py
@@ -4,12 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index_page():
-    # Return this as a strange
-    # return "<html><body>This is the homepage.</body></html>"
-
-    # Alternately, we could make this a Jinja template in `templates/`
-    # and return that result of rendering this, like:
-    #
     return render_template("index.html")
 
 @app.route("/application")
